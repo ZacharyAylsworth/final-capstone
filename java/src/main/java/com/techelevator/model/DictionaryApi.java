5a15c531d44a02c71369dac412506f7c793c904b
Ana Vazquez <ana.c.vazq@gmail.com>
2022-04-12 13:27:04 -0400

    categories model created

--- java/src/main/java/com/techelevator/model/DictionaryApi.java
+++ java/src/main/java/com/techelevator/model/DictionaryApi.java
@@ -1,5 +1,10 @@
 package com.techelevator.model;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 public class DictionaryApi {
