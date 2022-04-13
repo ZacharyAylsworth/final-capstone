1a5349ce5ca5c915c3d781bb95b80e135855fa75
Ana Vazquez <ana.c.vazq@gmail.com>
2022-04-13 09:27:19 -0400

    Dictionary Service + Model WIP

--- java/src/main/java/com/techelevator/model/DictionaryApi.java
+++ java/src/main/java/com/techelevator/model/DictionaryApi.java
@@ -1,6 +1,31 @@
 package com.techelevator.model;
 
 
+import java.util.Map;
+
 public class DictionaryApi {
 
+    private String word;
+    private Map<String, String> definitions;
+
+    public DictionaryApi() {
+        //empty
+    }
+
+    public DictionaryApi(String word, Map<String, String> definitions) {
+        this.word = word;
+        this.definitions = definitions;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public Map<String, String> getDefinitions() {
+        return definitions;
+    }
 }
