9bf9016b74f14de7582cd89e28c3b9572fe94fb5
Ana Vazquez <ana.c.vazq@gmail.com>
2022-04-18 13:12:08 -0400

    sync

--- java/src/main/java/com/techelevator/controller/ApiDecksController.java
+++ java/src/main/java/com/techelevator/controller/ApiDecksController.java
@@ -1,27 +1,12 @@
 package com.techelevator.controller;
-import com.techelevator.dao.FlashCardsDao;
-import com.techelevator.model.*;
-import com.techelevator.model.exceptions.CardNotFoundException;
-import com.techelevator.model.exceptions.CategoryNotFoundException;
-import com.techelevator.model.exceptions.DeckNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-public class ApiDecksController {
-    private FlashCardsDao dao;
+public class APIDecksController {
 
-    public ApiDecksController(FlashCardsDao dao) {
-        this.dao = dao;
-    }
 
-    @PreAuthorize("isAuthenticated()")
-    @RequestMapping(path = "/cards/observing", method = RequestMethod.POST)
-    public boolean saveCardObserving() {
-        return dao.saveCardObserving();
-    }
+
 }
